d4a6b9f2c830805b62260f551fc57a28ca7146c9
icaboalo <icaboalo@gmail.com>
2015-10-07 18:35:06 -0500

    Created SupportBar and applied it to MainActivity

--- app/src/main/java/com/icaboalo/historystoreapp/ui/fragment/CaptureListFragment.java
+++ app/src/main/java/com/icaboalo/historystoreapp/ui/fragment/CaptureListFragment.java
@@ -3,6 +3,7 @@ package com.icaboalo.historystoreapp.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,13 +30,15 @@ public class CaptureListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return
+                inflater.inflate(R.layout.fragment_capture_list, container, false);
+
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ButterKnife.bind(this, getActivity());
+        ButterKnife.bind(this, view);
     }
 
     @Override
@@ -51,7 +54,9 @@ public class CaptureListFragment extends Fragment {
     }
 
     private void setUpRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         CaptureRecyclerAdapter captureRecyclerAdapter = new CaptureRecyclerAdapter(createCaptureList(), getActivity());
         mCaptureRecyclerView.setAdapter(captureRecyclerAdapter);
+        mCaptureRecyclerView.setLayoutManager(linearLayoutManager);
     }
 }
