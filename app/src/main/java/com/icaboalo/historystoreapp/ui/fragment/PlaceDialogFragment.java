6ce403e6651d20c40547454f922c348bcffb6377
icaboalo <icaboalo@gmail.com>
2015-10-13 11:14:58 -0500

    added products to capture list fragment

--- app/src/main/java/com/icaboalo/historystoreapp/ui/fragment/PlaceDialogFragment.java
+++ app/src/main/java/com/icaboalo/historystoreapp/ui/fragment/PlaceDialogFragment.java
@@ -1,25 +1,38 @@
 package com.icaboalo.historystoreapp.ui.fragment;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.icaboalo.historystoreapp.R;
+import com.icaboalo.historystoreapp.util.VUtil;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by icaboalo on 10/12/2015.
  */
 public class PlaceDialogFragment extends DialogFragment {
 
+    @Bind(R.id.new_place_input)
+    EditText mNewPlaceInput;
+
+    @Bind(R.id.new_vendor_input)
+    EditText mNewVendorInput;
+
     public PlaceDialogFragment() {
 
     }
 
-    public static PlaceDialogFragment newInstance(String title){
+    public static PlaceDialogFragment newInstance(String title) {
         PlaceDialogFragment fragment = new PlaceDialogFragment();
         Bundle args = new Bundle();
         args.putString("Add Place", title);
@@ -33,9 +46,26 @@ public class PlaceDialogFragment extends DialogFragment {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.fragment_places_dialog, null);
-        alertDialog.setCancelable(false);
-        alertDialog.setTitle("Add Place");
-        alertDialog.setView(view);
+        ButterKnife.bind(this, view);
+        alertDialog.setCancelable(true)
+                .setView(view)
+                .setTitle("Add Place");
+        alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String newPlace = VUtil.extractText(mNewPlaceInput);
+                Toast.makeText(getActivity(), newPlace, Toast.LENGTH_SHORT).show();
+
+                dialog.dismiss();
+            }
+        });
+        alertDialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
 
 
         return alertDialog.create();
--- app/src/main/java/com/icaboalo/historystoreapp/util/VUtil.java
+++ app/src/main/java/com/icaboalo/historystoreapp/util/VUtil.java
@@ -2,6 +2,7 @@ package com.icaboalo.historystoreapp.util;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.widget.EditText;
 
 import com.icaboalo.historystoreapp.R;
 
@@ -9,6 +10,11 @@ import com.icaboalo.historystoreapp.R;
  * Created by icaboalo on 10/12/2015.
  */
 public class VUtil {
+
+    public static String extractText(EditText editText){
+        return editText.getText().toString();
+    }
+
     public static void replaceFragment(Fragment fragment, FragmentManager fragmentManager) {
         fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
     }
