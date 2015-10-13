a35d6820317636949f0abafaaf79064283c19227
icaboalo <icaboalo@gmail.com>
2015-10-12 21:34:46 -0500

    added menu for places fragment

--- app/src/main/java/com/icaboalo/historystoreapp/ui/activity/EmptyFragmentActivity.java
+++ app/src/main/java/com/icaboalo/historystoreapp/ui/activity/EmptyFragmentActivity.java
@@ -1,11 +1,8 @@
 package com.icaboalo.historystoreapp.ui.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.icaboalo.historystoreapp.R;
 import com.icaboalo.historystoreapp.ui.fragment.PlacesFragment;
@@ -30,29 +27,4 @@ public class EmptyFragmentActivity extends AppCompatActivity {
         VUtil.replaceFragment(new PlacesFragment(), getSupportFragmentManager());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_empty, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id){
-            case R.id.action_settings:
-                return true;
-            case R.id.action_save:
-                NavUtils.navigateUpFromSameTask(this);
-                finish();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/icaboalo/historystoreapp/ui/fragment/PlacesFragment.java
+++ app/src/main/java/com/icaboalo/historystoreapp/ui/fragment/PlacesFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -27,6 +30,12 @@ public class PlacesFragment extends Fragment implements PlacesRecyclerAdapter.My
 
     @Bind(R.id.places_list)
     RecyclerView mPlacesRecyclerView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Nullable
     @Override
@@ -63,5 +72,28 @@ public class PlacesFragment extends Fragment implements PlacesRecyclerAdapter.My
     @Override
     public void onMyClick(View item) {
         VUtil.replaceFragment(new AddCaptureFragment(), getFragmentManager());
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_settings:
+                return true;
+            case R.id.action_add_capture:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
