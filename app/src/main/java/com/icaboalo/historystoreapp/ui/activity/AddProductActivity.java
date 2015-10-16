79c32a5b7eefa3a1b7e33aac0f4ca2dc88c18205
icaboalo <icaboalo@gmail.com>
2015-10-16 09:28:50 -0500

    deleted snack bar

--- app/src/main/java/com/icaboalo/historystoreapp/ui/activity/AddProductActivity.java
+++ app/src/main/java/com/icaboalo/historystoreapp/ui/activity/AddProductActivity.java
@@ -2,7 +2,6 @@ package com.icaboalo.historystoreapp.ui.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -48,8 +47,6 @@ public class AddProductActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
                 showDialog();
             }
         });
